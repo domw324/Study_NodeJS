6a9f5e5b31b15bb48e4c4adfc6713d821eeeb895
unknown <domw324@gmail.com>
2019-08-11 23:24:23 +0900

    파일 create/update/delete기능 구현

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function temlateHTML(title, list, body){
+function temlateHTML(title, list, body, control){
     return `
     <!doctype html>
     <html>
@@ -13,6 +14,7 @@ function temlateHTML(title, list, body){
         <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
+            ${control}
             ${body}
         </body>
     </html>
@@ -21,7 +23,7 @@ function temlateHTML(title, list, body){
 
 function temlateList(filelist){
     var list = '<ul>';
-    var i = 0;
+    var i = 0;``
     while(i < filelist.length){
         list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
         i = i + 1;
@@ -36,13 +38,15 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
 
+    console.log(pathname);
+
     if(pathname === '/') {
         if(queryData.id === undefined) {
             fs.readdir('./data', function(error, filelist){
                 var title = "Welcome";
                 var description = 'Hello, Node.js';
                 var list = temlateList(filelist);
-                var template = temlateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+                var template = temlateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="create">create</a>`);
 
                 response.writeHead(200);
                 response.end(template);
@@ -53,13 +57,113 @@ var app = http.createServer(function(request,response){
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                     var title = queryData.id;
                     var list = temlateList(filelist);
-                    var template = temlateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+                    var template = temlateHTML(title, list,
+                        `<h2>${title}</h2><p>${description}</p>`,
+                        `<a href="create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="/delete_process" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>`
+                    );
 
                     response.writeHead(200);
                     response.end(template);
                 });
             });
         }       
+    } else if(pathname === '/create') {
+        fs.readdir('./data', function(error, filelist){
+            var title = "WEB - create";
+            var list = temlateList(filelist);
+            var template = temlateHTML(title, list, `
+                <form action="/create_process" method="post">
+                    <p><input type="text" name="title" placeholder="Title"></p>
+                    <p>
+                        <textarea name="description" placeholder="Description"></textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                </form>
+            `, '');
+
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else if(pathname === '/create_process') {
+        var body = '';
+        request.on('data', function(data){
+            body += data;
+            // if (body.length > 1e6)
+            // request.connection.destroy(); // 보안 장치. 너무 큰 정보 받은 경우 연결 끊음.
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                response.writeHead(302, {Location : `/?id=${title}`}); // re-direction, 처리 후 사용자를 다른 경로로 보낸다.
+                response.end();
+            });
+        });
+    } else if(pathname === '/update') {
+        fs.readdir('./data', function(error, filelist){
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                var title = queryData.id;
+                var list = temlateList(filelist);
+                var template = temlateHTML(title, list,
+                    `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p><input type="text" name="title" placeholder="Title" value="${title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="Description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                    `,
+                    `<a href="create">create</a> <a href="/update?id=${title}">update</a>`);
+
+                response.writeHead(200);
+                response.end(template);
+            });
+        });
+    } else if(pathname === '/update_process') {
+        var body = '';
+        request.on('data', function(data){
+            body += data;
+            // if (body.length > 1e6)
+            // request.connection.destroy(); // 보안 장치. 너무 큰 정보 받은 경우 연결 끊음.
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function(err){
+                fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                    response.writeHead(302, {Location : `/?id=${title}`});
+                    response.end();
+                });
+            });
+        });
+    } else if(pathname === '/delete_process') {
+        var body = '';
+        request.on('data', function(data){
+            body += data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            console.log(id);
+            fs.unlink(`data/${id}`, function(err){
+                response.writeHead(302, {Location : `/`});
+                response.end();
+            });
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
